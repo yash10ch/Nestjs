9269e6f60c3d7dff44b78c398680da1ccb121aa7
Yash Chaudhary <yashchaudhary.ch10@gmail.com>
2025-01-08 18:25:19 +0530

    update with database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,28 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
 
 
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [UsersModule, PostsModule, AuthModule, 
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: ()=>({
+        type: 'postgres',
+        entities :[User],
+        synchronize : true,
+        port: 5432,
+        host: 'localhost',
+        username: 'postgres',
+        password: '2510',
+        database: 'nestjs-blog',
+      })
+   
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
